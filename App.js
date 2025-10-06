9a06a0fa3a2d3f2c3a7c19c53ffe33a466330272
Your Name <you@example.com>
2025-10-06 23:59:07 +0530

    Fixed ReactDOM import and removed main field from package.json

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-const parent = React.createElement('div',{id:'parent'},React.createElement('div',{id:'child'},[React.createElement('h1',{},'I am h1 tag'),React.createElement('h2',{},'I am h2 tag')])); 
-// it returns object
-//it is a nested structure of objects
-// React.createElement(tag,props,children)
-// to get two h1 we need to pass them in array
+import React from 'react'; // it is used to create react element
+import ReactDOM from 'react-dom/client'; // it is used to render react element on screen
 
-const heading = React.createElement('h1',{id:'heading'},'Hello,world!'); // returns object 
+//React.createElement = reactelement(object) = HTMLElement(render)
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // it takes html element as argument
+const heading = React.createElement("h1",{id:"heading"}, "Hello World from React");
 
-root.render(parent); // it converts object to html and shows on screen 
+//JSX React - is not html in js , it is html like syntax .
+// jsx (trnspiled) before going to browser it is converted into react element by babel
+// babel is a js compiler it converts jsx to react element
+// babel is used in parcel by default we don't need to install it
+const jsxheading = <h1 id="heading">Hello World from JSX</h1> // it is also a react element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxheading); // it will render the react element on screen
---
+++ README.md
@@ -0,0 +1,20 @@
+...
+# parcel
+- Dev Build
+- local Server
+- HMR = Hot Module Replacement -> for when we save the file it will refresh the localhost .
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Images Optimization 
+- IN production - Minification
+- Bundling
+- Compressing
+- parcel is manages all the packages 
+- Consistent Hashing -> hw
+- Code splitting
+- Differential Bundling - when our app is made and can be opened anywhere or on which browser older or newer . 
+- Diagnostic of file . Beautifull errors 
+- Error Handling 
+- It helps also to host on the https .
+- Tree shaking - It will remove unused code in the file which is not required .
+- Different for dev and production .
